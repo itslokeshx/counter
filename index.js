e85e18fab2d618b3a2c93a9e950ff09458c19d80
LOKESH A <131357693+369loki@users.noreply.github.com>
2023-09-05 11:18:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
+var count = 0;
 var  increase = document.querySelector(".incre");
 var decrease =  document.querySelector(".dec");
 var reset =  document.querySelector(".reset");
 
 increase.addEventListener("click",function(){
+   count += 1;
+   document.querySelector("p").innerHTML = count;
+   if(count >0 ){
     document.querySelector("p").style.color = "rgb(6, 211, 23)";
-    var one = 1;
-   var count=  document.querySelector("p").innerHTML -= (one)*(-1);
-   count1 =eval(count);
-   return document.querySelector("p").innerHTML = count1;
+}
+else if(count < 0){
+    document.querySelector("p").style.color = "rgb(216, 23, 23)";  
+}
+else if(count === 0){
+    document.querySelector("p").style.color = "hsl(205, 79%, 22%)";
+}      
 })
-
 
 reset.addEventListener("click",function(){
-    document.querySelector("p").style.color = "hsl(205, 79%, 22%)";
-   return document.querySelector("p").innerHTML = 0;
-
-})
-
-decrease.addEventListener("click",function(){
+    count = 0;
+ document.querySelector("p").innerHTML = count;
+ if(count >0 ){
+    document.querySelector("p").style.color = "rgb(6, 211, 23)";
+}
+else if(count < 0){
     document.querySelector("p").style.color = "rgb(216, 23, 23)";  
-    var one = 1;
-   var count=  document.querySelector("p").innerHTML -= one;
-   count1 =eval(count);
-   return document.querySelector("p").innerHTML = count1;                      
+}
+else if(count === 0){
+    document.querySelector("p").style.color = "hsl(205, 79%, 22%)";
+}      
+})
+decrease.addEventListener("click",function(){
+    count -= 1;
+  document.querySelector("p").innerHTML = count;        
+   if(count >0 ){
+    document.querySelector("p").style.color = "rgb(6, 211, 23)";
+}
+else if(count < 0){
+    document.querySelector("p").style.color = "rgb(216, 23, 23)";  
+}
+else if(count === 0){
+    document.querySelector("p").style.color = "hsl(205, 79%, 22%)";
+} 
 })
